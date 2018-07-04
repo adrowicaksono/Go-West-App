af9581c18f5066f8ab6a6b2333ad4c02ccceb761
adrowicaksono <adrowicaksono@gmail.com>
2018-07-04 11:50:13 +0700

    modified nodels/bike.js

--- models/bike.js
+++ models/bike.js
@@ -1,10 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Bike = sequelize.define('Bike', {
-    status: DataTypes.STRING,
-    tag: DataTypes.STRING,
-    vendorId: DataTypes.INTEGER,
-    terminalId: DataTypes.INTEGER,
+      status: DataTypes.STRING,
+      tag: DataTypes.STRING,
+      category : DataTypes.STRING,
+      vendorId: DataTypes.INTEGER,
+      terminalId: DataTypes.INTEGER,
   }, {});
   Bike.associate = function(models) {
     // associations can be defined here
